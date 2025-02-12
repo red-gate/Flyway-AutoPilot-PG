8fd1c505cff5e7daa79a2f437ad7f976d1166c92
Chris Hawkins <94678272+csnhawkins@users.noreply.github.com>
2025-02-12 10:24:18 +0000

    Added additional comments to run step 2 after the databases have been created

--- Scripts/1. CreateAutoPilotDatabases.sql
+++ Scripts/1. CreateAutoPilotDatabases.sql
@@ -9,3 +9,5 @@ create database autopilot_prod with encoding 'UTF8';
 create database autopilot_check with encoding 'UTF8';
 create database autopilot_shadow with encoding 'UTF8';
 create database autopilot_build with encoding 'UTF8';
+
+-- Once the databases have been created, run the script '2. CreateAutoPilotInitialSchemas.sql' against the autopilot_dev database
